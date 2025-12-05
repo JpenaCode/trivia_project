58440f82ce0db8aa914c58bb83210747f3c6ed65
esegev1 <esegev1@gmail.com>
2025-12-04 22:06:49 -0500

    added params to question path

--- trivia_api/views.py
+++ trivia_api/views.py
@@ -1,29 +1,24 @@
 from django.shortcuts import render
-
 from rest_framework import generics
 from .models import Question, User
 from .serializers import QuestionSerializer, UserSerializer
 
 # Question views
 class QuestionList(generics.ListCreateAPIView):
-    queryset = Question.objects.all().order_by('id')
     serializer_class = QuestionSerializer
-	# def get_queryset(self):
-	# 	print("--- ContactList: Executing GET request. ---")
-	# 	return super().get_queryset()
-
-	# def post(self, request, *arg, **kwargs) :
-	# 	print(f"--- ContactList: Executing POST request with data: {reques.data} ---")
-	# 	return super().post(request, *args, **kwards)
-
-	# def get_queryset(self):
-	# 	# Print to confirm GET request reached here
-	# 	print("--- ContactList: Executing GET request. ---")
-	# 	return super().get_queryset()
-	# def post(self, request, *args, **kwargs):
-	# 	# Print to confirm POST request reached here, and show incoming data
-	# 	print(f"--- ContactList: Executing POST request with data: {request.data} ---")
-	# 	return super().post(request, *args, **kwargs)
+    
+    def get_queryset(self):
+        queryset = Question.objects.all().order_by('id')  # You forgot this line!
+        
+        category = self.kwargs.get('category')
+        difficulty = self.kwargs.get('difficulty')
+        
+        if category:
+            queryset = queryset.filter(category=category)
+        if difficulty:
+            queryset = queryset.filter(difficulty=difficulty)
+        
+        return queryset 
 
 class QuestionDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Question.objects.all().order_by('id')
